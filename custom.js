922301ea47231ee45252752f2addf803e8caa918
Fabrizio Azzarri <fabrizio.azzarri@gmail.com>
2020-11-19 15:52:11 +0100

    Initial commit

---
+++ custom.js
@@ -0,0 +1,10 @@
+function closeMenu() {
+    var desktopHeader = document.getElementById("desktop-header");
+    var mobileHeader = document.getElementById("mobile-header");
+    var main = document.getElementById("main");
+    var hamburger = document.getElementById("hamburger");
+    desktopHeader.classList.toggle("d-none");
+    mobileHeader.classList.toggle("open");
+    main.classList.toggle("open");
+    hamburger.classList.toggle("is-active");
+}
