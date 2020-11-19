f2a9ba832110628e6df579ec490955e0cea32930
Fabrizio Azzarri <fabrizio.azzarri@gmail.com>
2020-11-19 17:34:24 +0100

    bugfix on menu opening , added viewport width calculation

--- custom.js
+++ custom.js
@@ -3,8 +3,13 @@ function closeMenu() {
     var mobileHeader = document.getElementById("mobile-header");
     var main = document.getElementById("main");
     var hamburger = document.getElementById("hamburger");
-    desktopHeader.classList.toggle("d-none");
-    mobileHeader.classList.toggle("open");
-    main.classList.toggle("open");
-    hamburger.classList.toggle("is-active");
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+    if (vw <= 768) {
+        desktopHeader.classList.toggle("d-none");
+        mobileHeader.classList.toggle("open");
+        main.classList.toggle("open");
+        hamburger.classList.toggle("is-active");
+    }
+
 }
--- dist/custom.dev.js
+++ dist/custom.dev.js
@@ -5,8 +5,12 @@ function closeMenu() {
   var mobileHeader = document.getElementById("mobile-header");
   var main = document.getElementById("main");
   var hamburger = document.getElementById("hamburger");
-  desktopHeader.classList.toggle("d-none");
-  mobileHeader.classList.toggle("open");
-  main.classList.toggle("open");
-  hamburger.classList.toggle("is-active");
+  var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+  if (vw <= 768) {
+    desktopHeader.classList.toggle("d-none");
+    mobileHeader.classList.toggle("open");
+    main.classList.toggle("open");
+    hamburger.classList.toggle("is-active");
+  }
 }
